1a00fd9a0e760f5850e6e62e3fc1021f7b448d63
gringrape <gringrape200@gmail.com>
2020-06-16 19:50:29 +0900

    UI 로직
    - state 결정 -> 위 글자 단어, 아래 글자 단어, 아래 글자 색
    - utils -> 무작위 숫자 추출
    - colorRepo -> 색깔 정보

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // states
+  const [upperWordNumber, setUpperWordNumber] = useState(3);
+  const [lowerWordNumber, setLowerWordNumber] = useState(2);
+  const [lowerColorNumber, setLowerColorNumber] = useState(3);
+
+  // computation
+  const upperWord = utils.colorWords[upperWordNumber];
+  const lowerWord = utils.colorWords[lowerWordNumber];
+
   return (
     <div className="game">
       <h1 className="game-title">색깔 맞추기</h1>
@@ -11,8 +20,8 @@ function App() {
           <img className="game-box__answer game-box__answer--right" src={require("./correct.png")} alt="correct-img"/>                
           <img className="game-box__answer game-box__answer--wrong" src={require("./cancel.png")} alt="correct-img"/>                
         </div>
-        <div className="game-box__word">빨강</div>
-        <div className="game-box__word">파랑</div>
+        <div className="game-box__word">{upperWord}</div>
+        <div className="game-box__word" style={{color: utils.colors[lowerColorNumber]}}>{lowerWord}</div>
         <button className="game-box__btn">네</button>
         <button className="game-box__btn">아니오</button>
       </div>
@@ -23,6 +32,19 @@ function App() {
       </footer>
     </div>
   );
+}
+
+const colorRepo = {
+  // colors 와 colorWords 는 index 가 같으면 같은 색을 의미해야 합니다.
+  count: 7, 
+  colorWords: ["빨강", "주황", "노랑", "초록", "파랑", "남색", "보라"],
+  colors: ['red', 'orange', 'yellow', 'green', 'blue', 'navy', 'purple']
+}
+
+const utils = {
+  random: () => {
+    return Math.floor(Math.random() * this.count);
+  }
 }
 
 export default App;
