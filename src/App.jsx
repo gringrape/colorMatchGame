0cbb06087f36b6b4aa43584e7de35735ddb1046b
gringrape <gringrape200@gmail.com>
2020-06-16 15:16:19 +0900

    initial markup
    - game title
    - game description
    - box
    - button
    - word

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="game">
+      <h1 className="game-title">색깔 맞추기</h1>
+      <p className="game-description">아래 단어의 색깔이 위에 쓰여진 글자와 일치하나요?</p>
+      <div className="game-box">
+        <div className="game-box__word">빨강</div>
+        <div className="game-box__word">파랑</div>
+        <button className="game-box__btn">네</button>
+        <button className="game-box__btn">아니오</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
