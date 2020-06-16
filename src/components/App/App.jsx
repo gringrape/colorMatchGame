f2e846f7e1016530028cb8a3541dc6a4cc117139
gringrape <gringrape200@gmail.com>
2020-06-16 21:29:25 +0900

    스크린 추가
    - 정답과 오답일 경우 스크린 작동 로직 추가

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,39 +4,43 @@ import colorRepo from '../../colorRepo';
 import utils from '../../utils';
 import DisplayWord from '../DisplayWord';
 import Footer from '../Footer';
+import Screen from '../Screen';
 
 function App() {
   // states
-  const [upperWordNumber, setUpperWordNumber] = useState(3);
-  const [lowerWordNumber, setLowerWordNumber] = useState(1);
-  const [lowerColorNumber, setLowerColorNumber] = useState(3);
+  const [upperWordNumber, setUpperWordNumber] = useState(utils.random(colorRepo.count));
+  const [lowerWordNumber, setLowerWordNumber] = useState(utils.random(colorRepo.count));
+  const [lowerColorNumber, setLowerColorNumber] = useState(utils.random(colorRepo.count));
+  const [gameStatus, setGameStatus] = useState('playing');
 
   // computation
   const upperWord = colorRepo.colorWords[upperWordNumber];
   const lowerWord = colorRepo.colorWords[lowerWordNumber];
   const lowerColor = colorRepo.colors[lowerColorNumber];
 
+  const checkAnswerIsEqual = (right) => {
+    return () => {
+      (upperWordNumber === lowerColorNumber) === right
+        ? setGameStatus('win')
+        : setGameStatus('lose'); 
+      // TODO need to update status
+    }  
+  }
+
   return (
     <div className="game">
       <h1 className="game-title">색깔 맞추기</h1>
       <p className="game-description">아래 단어의 색깔이 위에 쓰여진 글자와 일치하나요?</p>
       <div className="game-box">
-        <div className="game-box__screen">
-          <img 
-            className="game-box__answer game-box__answer--right" 
-            src={require("../../images/correct.png")} 
-            alt="correct-img"
-          />                
-          <img 
-            className="game-box__answer game-box__answer--wrong" 
-            src={require("../../images/cancel.png")} 
-            alt="correct-img"
-          />                
-        </div>
+        {
+          gameStatus === 'playing'
+            ? <></>
+            : <Screen status={gameStatus} />
+        }
         <DisplayWord word={upperWord} />
         <DisplayWord word={lowerWord} color={lowerColor} />
-        <button className="game-box__btn">네</button>
-        <button className="game-box__btn">아니오</button>
+        <button className="game-box__btn" onClick={checkAnswerIsEqual(true)}>네</button>
+        <button className="game-box__btn" onClick={checkAnswerIsEqual(false)}>아니오</button>
       </div>
       <Footer />
     </div>
